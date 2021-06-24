a147797197084b277efb44848b5a93589c2334c0
shafqat <45786769+sjkbnx@users.noreply.github.com>
2021-06-24 16:46:01 +0500

    export issues resolved

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import Bus from './Event'
 import StartNarration from './NarrationService'
 
-module.exports.Bus = Bus;
-module.exports.StartNarration = StartNarration;
+export {
+    Bus,
+    StartNarration
+}
